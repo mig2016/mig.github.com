00d7e53a041c9881f7d8d6c900e87d7119011dde
mig2016 <miracle.z28@gmail.com>
2017-01-07 12:55:49 -0500

    Add files via upload

---
+++ sendemail.php
@@ -0,0 +1,26 @@
+<?php
+
+        $name = @trim(stripslashes($_POST['name']));
+        $email = @trim(stripslashes($_POST['email']));
+        $subject = @trim(stripslashes($_POST['phn']));
+        $message = @trim(stripslashes($_POST['message']));
+
+        $email_from = $email;
+        $email_to = 'jarrod@miracleinvestmentgroup.com';
+        $subject = 'Form miracleinvestmentgroup.com';
+
+        $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Phone: ' . $subject . "\n\n" . 'Message: ' . $message;
+
+         $success = @mail($email_to, $subject, $body);
+
+    ?>
+
+    <!DOCTYPE HTML>
+    <html lang="en-US">
+
+    <head>
+        <script>
+            alert("Thank you for getting in touch!");
+        </script>
+        <meta HTTP-EQUIV="REFRESH" content="0; url=http://miracleinvestmentgroup.com/">
+    </head>
